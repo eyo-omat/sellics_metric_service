9d26621314201acec9bc89eb07c241421b9c91f2
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-03-22 22:02:17 +0000

    :clown_face: initial commit

---
+++ src/main/java/com/sellics/sellics_metric_service/SellicsMetricServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sellics.sellics_metric_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellicsMetricServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellicsMetricServiceApplication.class, args);
+    }
+
+}
